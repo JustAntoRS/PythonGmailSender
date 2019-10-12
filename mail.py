0f7c29ed20137059e06934959fa87b032cf260f3
Alvaro Ferrero <alfedi3867@hotmail.com>
2019-10-12 23:05:01 +0000

    Add HTML support

--- mail.py
+++ mail.py
@@ -2,23 +2,26 @@ import smtplib
 import csv
 from email.message import EmailMessage
 
-#Your gmail credentials
+# MIME libraries
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+# Your gmail credentials
 gmail_name = 'your gmail account goes here'
 gmail_password = 'your password goes here'
 
-#Define the message
-with open('email.txt') as text:
-    msg = text.read()
+# Define the message
+msg = open('email.html').read()
 
-#Basic function to send mails
+    # Basic function to send mails
 def send_mail(body, subject, to):
-    msg = EmailMessage()
-    msg.set_content(body)
+    msg = MIMEMultipart()
+    msg.attach(MIMEText(body, 'html'))
     msg['Subject'] = subject
     msg['From'] = gmail_name
     msg['To'] = to
 
-    #Send the email
+    # Send the email
     try:
         server_ssl.send_message(msg)
     except:
@@ -27,7 +30,7 @@ def send_mail(body, subject, to):
     return
 ##########################################
 
-#Open the connection to the server and login into it
+# Open the connection to the server and login into it
 try:
     server_ssl= smtplib.SMTP_SSL('smtp.gmail.com', 465)
     server_ssl.login(gmail_name,gmail_password)
@@ -39,10 +42,11 @@ with open('to_example.csv') as csv_file:
     line_count = 0
 
     for row in csv_reader:
-        msg = msg.replace("{}",row[0],1)
-        send_mail(msg,'subject',row[1])
-        msg = msg.replace(row[0],"{}",1)
-    line_count += 1
+        if line_count != 0:
+            msg = msg.replace("{}",row[0],1)
+            send_mail(msg,'subject',row[1])
+            msg = msg.replace(row[0],"{}",1)
+        line_count += 1
 
-#Close the connection
+# Close the connection
 server_ssl.quit()
