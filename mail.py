eb81d20094e1141f96647ff18751cc91e03e29bc
JustAntoRS <justantorami@gmail.com>
2019-10-17 21:56:39 +0200

    Improved Alfedi's version <3

--- mail.py
+++ mail.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import smtplib
 import csv
 from email.message import EmailMessage
@@ -11,12 +13,22 @@ gmail_name = 'your gmail account goes here'
 gmail_password = 'your password goes here'
 
 # Define the message
-msg = open('email.html').read()
+bodyHTML = open('email.html').read()
+bodyPLAIN = open('email.txt').read()
 
     # Basic function to send mails
-def send_mail(body, subject, to):
-    msg = MIMEMultipart()
-    msg.attach(MIMEText(body, 'html'))
+def send_mail(body_html, body_plan, subject, to):
+    msg = MIMEMultipart('mixed')
+    msgR = MIMEMultipart('related')
+    msgA = MIMEMultipart('alternative')
+
+    # Add HTML and plain text version of the mail
+    msgR.attach(MIMEText(bodyHTML, 'html'))
+    msgA.attach(MIMEText(bodyPLAIN, 'plain'))
+
+    msgA.attach(msgR)
+    msg.attach(msgA)
+
     msg['Subject'] = subject
     msg['From'] = gmail_name
     msg['To'] = to
@@ -26,8 +38,6 @@ def send_mail(body, subject, to):
         server_ssl.send_message(msg)
     except:
         print('Impossible to send the mail')
-
-    return
 ##########################################
 
 # Open the connection to the server and login into it
@@ -43,9 +53,15 @@ with open('to_example.csv') as csv_file:
 
     for row in csv_reader:
         if line_count != 0:
-            msg = msg.replace("{}",row[0],1)
-            send_mail(msg,'subject',row[1])
-            msg = msg.replace(row[0],"{}",1)
+            # Personalize email with name
+            bodyHTML = bodyHTML.replace("{}", row[0], 1)
+            bodyPLAIN = bodyPLAIN.replace("{}", row[0], 1)
+
+            send_mail(bodyHTML, bodyPLAIN, 'subject', row[1])
+
+            # Get back non-personalize version
+            bodyHTML = bodyHTML.replace(row[0], "{}", 1)
+            bodyPLAIN = bodyPLAIN.replace(row[0], "{}", 1)
         line_count += 1
 
 # Close the connection
